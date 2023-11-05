e0316c8d107ab4effb6540baf6c46512499e4e9c
tomo-Y2002 <tomohiro2014yamashita@gmail.com>
2023-11-05 23:17:02 +0900

    CORSを適応した。

--- go/main.go
+++ go/main.go
@@ -64,7 +64,7 @@ func init() {
 }
 
 func handlerUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Origin", "https://uttc-hackathon-frontend-chi.vercel.app/")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	switch r.Method {
@@ -185,7 +185,7 @@ func handlerUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlerUsers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Origin", "https://uttc-hackathon-frontend-chi.vercel.app/")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 	switch r.Method {
 	case http.MethodGet:
